baf72394da22b54646b23c92e0baf01f579af92b
TheTrueMCGamer <46852093+TheTrueMCGamer@users.noreply.github.com>
2025-08-24 12:28:06 -0500

    Update Commands.cs

--- DiscordBotPlugin/Commands.cs
+++ DiscordBotPlugin/Commands.cs
@@ -47,7 +47,7 @@ namespace DiscordBotPlugin
         /// </summary>
         /// <param name="msg">Command to send to the server</param>
         /// <returns>Task</returns>
-        /*public Task SendConsoleCommand(SocketSlashCommand msg)
+        public Task SendConsoleCommand(SocketSlashCommand msg)
         {
             try
             {
@@ -92,7 +92,7 @@ namespace DiscordBotPlugin
                 // Return a completed task to fulfill the method signature
                 return Task.CompletedTask;
             }
-        */}
+        }
 
         /// <summary>
         /// Send a chat message to the AMP instance, only for Minecraft for now
@@ -100,7 +100,7 @@ namespace DiscordBotPlugin
         /// <param name="author">Discord name of the sender</param>
         /// <param name="msg">Message to send</param>
         /// <returns>Task</returns>
-        /*public Task SendChatCommand(string author, string msg)
+        public Task SendChatCommand(string author, string msg)
         {
             try
             {
@@ -135,7 +135,7 @@ namespace DiscordBotPlugin
                 // Return a completed task to fulfill the method signature
                 return Task.CompletedTask;
             }
-        }*/
+        }
 
         /// <summary>
         /// Hook into LocalFileBackupPlugin events and request a backup
